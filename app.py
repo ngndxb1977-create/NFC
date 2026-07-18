cf8d53737a482da775dd26b53c0122445b44b8a7
ngndxb1977-create <ngndxb1977@gmail.com>
2026-07-18 13:43:27 +0400

    Update app.py

--- app.py
+++ app.py
@@ -221,7 +221,7 @@ def load_all_vehicle_data(vehicle_file_path):
 # ------------------------------------------------------------------
 # CONFIG & FILE TARGETS
 # ------------------------------------------------------------------
-FILE_VEHICLES = "NFC New VRI Project (2).xlsx"  # Updated to match your exact file name
+FILE_VEHICLES = "NFC New VRI Project (2) (2).xlsx"  # Updated to match your exact file name
 FILE_SUPPLEMENT = "Bank & RMC Details.xlsx"
 
 VEHICLE_CATALOG = load_all_vehicle_data(FILE_VEHICLES)
